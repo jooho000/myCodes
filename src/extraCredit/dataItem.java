b85c79ce83ff272387bb4b8d295bf4ca21d9d998
jooho000 <70412279+jooho000@users.noreply.github.com>
2023-10-17 17:37:42 -0700

    Add files via upload

---
+++ src/extraCredit/dataItem.java
@@ -0,0 +1,13 @@
+package extraCredit;
+
+public class dataItem {
+    public int dData;
+
+    public dataItem(int data) {
+        dData = data;
+    }
+
+    public void displayItem() {
+        System.out.print(" " + dData);
+    }
+}
